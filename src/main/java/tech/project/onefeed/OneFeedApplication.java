057290557bd83608c77799ae7ee1aeb51dd9dc95
ricardom <ricko62291@gmail.com>
2022-04-26 11:59:09 -0500

    initial commit

---
+++ src/main/java/tech/project/onefeed/OneFeedApplication.java
@@ -0,0 +1,13 @@
+package tech.project.onefeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/project/onefeed/OneFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.project.onefeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
