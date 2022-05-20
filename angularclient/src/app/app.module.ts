fadb214ac70f5c9745a1eddbb4d3a621d256ecd2
Jordan Green <contact@jgreen.io>
2022-05-20 15:06:46 -0500

    generate angular components

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ProfileComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
